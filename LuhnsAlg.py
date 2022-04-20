bcb3ae375cf30a9c07f51151979f04df7d802540
hopsonj19 <55367839+hopsonj19@users.noreply.github.com>
2022-04-20 16:23:47 -0500

    Update LuhnsAlg.py
    Added input sanitization, clarified the output, and optimized the looping.

--- LuhnsAlg.py
+++ LuhnsAlg.py
@@ -1,6 +1,7 @@
-def Luhn_check():
-    string = input("Please enter your 16 digit card number: ")
-    # string = string.join("")
+def Luhn_check(string):
+    string = string.replace(" ", "")
+    if len(string) != 16:
+        return "Invalid number of digits."
     string = string[::-1]
     checkdigit = string[0]
     string = string.replace(checkdigit, '', 1)
@@ -23,9 +24,10 @@ def Luhn_check():
     if str(checksum) == checkdigit:
         return "Accept"
     else:
-        return "Invalid Credit Number"
+        return "Invalid Credit Card Number"
 
-contin = "Start"
-while contin != "Stop":
-    print(Luhn_check())
-    contin = input("Type 'Stop' if you are finished: ")
+string = input("Please enter your 16 digit card number: ")
+while string != "stop":
+    print(Luhn_check(string))
+    string = input("Enter another card number or type 'stop' if you are finished: ")
+    string = string.lower()
